0864f48bbdf4caf9e1e2e6348676faa2e0edaebe
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2026-06-16 20:20:42 +0900

    Update sw_test.ino

--- sw_test.ino
+++ sw_test.ino
@@ -1,20 +1,48 @@
-int SWL = 8;
-int SWR = 7;
+/*
+ * ==========================================
+ *  Bounce Bot - Bumper Sensor Test
+ * ==========================================
+ *  Author   : yangjipsa
+ *  Company  : TouchLabs (touchlabs.kr)
+ *  Version  : 1.0
+ *  Date     : 2026-06-16
+ *  Product  : Bounce Bot
+ *  Purpose  : Test code to read left/right bumper
+ *             switch states and print them to
+ *             the Serial Monitor.
+ *  Board    : Arduino Nano
+ *  Pin Map  : SWL -> D8, SWR -> D7
+ * ------------------------------------------
+ *  Copyright (c) 2026 TouchLabs.
+ *  All rights reserved.
+ *
+ *  This code is the property of TouchLabs and is
+ *  provided for the Bounce Bot product. Unauthorized
+ *  copying, distribution, or modification is prohibited.
+ * ==========================================
+ */
 
-void setup() 
+int SWL = 8;   // Left bumper switch pin
+int SWR = 7;   // Right bumper switch pin
+
+void setup()
 {
-  Serial.begin(9600);
-  pinMode(SWL, INPUT);
-  pinMode(SWR, INPUT);
+  Serial.begin(9600);     // Start serial communication
+  pinMode(SWL, INPUT);    // Set left switch as input
+  pinMode(SWR, INPUT);    // Set right switch as input
 }
 
-void loop() 
+void loop()
 {
+  // Read bumper switch states
   int BumperL = digitalRead(SWL);
   int BumperR = digitalRead(SWR);
-  Serial.print("Bumper L :"); 
+
+  // Print the values to the Serial Monitor
+  Serial.print("Bumper L :");
   Serial.print(BumperL);
   Serial.print(", Bumper R :");
   Serial.println(BumperR);
-  delay(100);
+
+  delay(100);   // Wait 0.1 second
 }
